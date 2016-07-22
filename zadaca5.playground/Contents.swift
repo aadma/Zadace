3ff60da689bc012f173e0287787c809ee85dc748
admir WIP <admirWIP@admirs-iMac.local>
2016-07-22 16:37:12 +0200

    ispravke

--- zadaca5.playground/Contents.swift
+++ zadaca5.playground/Contents.swift
@@ -16,19 +16,16 @@ var coords4 = Location()
 
 // ZADATAK 2
 class Parent: Person{
-    var children: [Person]?
+    var children: [Person]
     var savings: Double?
     init(name: String, lastName: String, yearOfBirth: Int, location: Location, children: [Person]){
         self.children = children
-        
         super.init(name: name, lastName: lastName, yearOfBirth: yearOfBirth, location: location)
     }
     convenience init(name: String, lastName: String){
         self.init(name: name, lastName: lastName, yearOfBirth: 1946, location: coords3, children: [Person]())
         self.children = [Person]()
-        
     }
-   
 }
 
 extension Int {
@@ -42,7 +39,6 @@ class Person{
     var lastName: String
     var yearOfBirth: Int
     var age: Int{
-        
         return 2016 - yearOfBirth
     }
     weak var father: Parent?
@@ -56,10 +52,9 @@ class Person{
     }
     convenience init(name: String, lastName: String){
         self.init(name: name, lastName: lastName, yearOfBirth: 1990, location: coords)
-        
     }
     func introduction()-> String{
-        return "Hi my name is \(name) \(lastName) age \(age)"
+        return "Hi my name is \(name) \(lastName) age \(age)."
     }
 }
 let mirko = Person(name: "Mirko", lastName: "Babic", yearOfBirth: 1987, location: coords)
@@ -67,20 +62,13 @@ let nedim = Person(name: "Nedim", lastName: "Sabic", yearOfBirth: 1982, location
 mirko.introduction()
 nedim.introduction()
 
-
-
-
 struct Course{
     var teacher: Person
     var courseName: String
     func aboutCourse()->String{
-        
         return  "\(courseName) course by prof. \(teacher.lastName)"
     }
 }
-
-
-
 
 class Student: Person{
     var attendingCourses: [Course]?
@@ -95,50 +83,46 @@ class Student: Person{
             return Double(total/grades.count)
         }
     }
-    init(name: String, lastName: String, yearOfBirth: Int, location: Location, attendingCourses: [Course]?, grades: [Int]){
+    init(name: String, lastName: String, yearOfBirth: Int, location: Location, attendingCourses: [Course], grades: [Int]){
         self.attendingCourses = attendingCourses
         self.grades = grades
         super.init(name: name, lastName: lastName, yearOfBirth: yearOfBirth, location: location)
     }
     
     override func introduction() -> String{
-        var introduceStudent = "Hi my name is \(name) \(lastName) age \(age)" + " I am a student at \(faculty)."
-        if attendingCourses != nil{
-            introduceStudent += "My favourite course is \(attendingCourses!.first!.aboutCourse())"
+    
+        var introduceStudent = " I am a student at \(faculty)."
+        if let attendingCourse = attendingCourses{
+            introduceStudent += "My favourite course is \(attendingCourse.first!.aboutCourse())."
         }
-        if let savings = super.father?.savings{
-            introduceStudent += ".My father has $\(savings) savings"
-        }else if let savings = super.mother?.savings{
-            introduceStudent += ".My mother has $\(savings) savings"
-        }else{
-            introduceStudent += ".My parents are broke"
+        if (father?.savings) == nil && (mother?.savings) == nil{
+            introduceStudent += "My parents are broke"
         }
-        return introduceStudent
+        if let fatherSavings = father?.savings{
+            introduceStudent += "My father has $\(fatherSavings) savings."
+        }
+        if let motherSavings = mother?.savings{
+            introduceStudent += "My mother has $\(motherSavings) savings."
+        }
+        return super.introduction() + introduceStudent
     }
     
 }
 
-
-
 var iosDevelopment = Course(teacher: mirko, courseName: "iOS development")
 var seo = Course(teacher: nedim, courseName: "SEO")
 var courses = [seo, iosDevelopment]
-
 let student = Student(name: "Admir", lastName: "Halep", yearOfBirth: 1983, location: coords3, attendingCourses: courses, grades: [9,10,9,8,10])
 student.introduction()
-
-
-
 let aida = Person(name: "Aida", lastName: "Durmisevic", yearOfBirth: 1972, location: coords4)
-var ismet: Parent?
-ismet = Parent(name: "Ismet", lastName: "Halep", yearOfBirth: 1946, location: coords3, children: [aida , student])
-var ilduza: Parent?
-ilduza = Parent(name: "Ilduza", lastName: "Halep", yearOfBirth: 1951, location: coords3, children: [aida, student])
-ismet!.savings = Double(arc4random() % 10000)
+var ismet = Parent(name: "Ismet", lastName: "Halep", yearOfBirth: 1946, location: coords3, children: [aida , student])
+var ilduza = Parent(name: "Ilduza", lastName: "Halep", yearOfBirth: 1951, location: coords3, children: [aida, student])
+ismet.savings = Double(arc4random() % 10000)
+ilduza.savings = Double(arc4random() % 10000)
 student.father = ismet
 student.mother = ilduza
-ismet!.savings
+ismet.savings
 student.introduction()
-ismet!.introduction()
+ismet.introduction()
 var days = student.age
 days.daysInAge()
