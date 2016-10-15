926ac15c6290057739d4819711f058efd6aa8639
aadma <aadma1@hotmail.com>
2016-10-15 22:35:35 +0200

    Update NewContactViewController.swift

--- Dopuna/Dopuna/NewContactViewController.swift
+++ Dopuna/Dopuna/NewContactViewController.swift
@@ -34,26 +34,7 @@ class NewContactViewController: UIViewController {
         numberTextfield.layer.cornerRadius = 5
         createContactButton.layer.cornerRadius = 5
     }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
-    }
-    
-
-    
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
-        // Get the new view controller using segue.destinationViewController.
-        // Pass the selected object to the new view controller.
-        if segue.identifier == "saveSegue"{
-        }
-        
-    }
-    
-    
+ 
     // MARK: Actions
     
     @IBAction func addContactBigButton(sender: UIButton) {
@@ -65,9 +46,7 @@ class NewContactViewController: UIViewController {
         addNewContact()
     }
     
-    
-    
-    func addNewContact(){
+     func addNewContact(){
         let name = nameTextField.text
         var number = numberTextfield.text ?? ""
         if number == ""{
